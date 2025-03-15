ae61c60c34ecef3bea2487fbfb604c517f09cc90
maiconjesus21 <maiconzica289@gmail.com>
2025-03-15 04:15:28 +0000

    adicionei barcos diagonais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+# define linhas 10
+# define colunas 10
 
 
- int main(){
-    
-    printf("novo commit\n");
+    int main(){
+
     char letras[10] = {'A','B','C','D','E','F','G','H','I','J'};  //declaração das variaveis
 
-   int numeros[10] = {1,2,3,4,5,6,7,8,9,10};
+   int numeros[10] = {1,2,3,4,5,6,7,8,9,10}; //letras do mapa 
    
    int mar[10][10] = {0};
    
-   int navio[3] =  {3,3,3};
+   int navio[3] =  {3,3,3};    //criação das arrays(navio)
    int navio2[3] = {3,3,3};
-
-
+   int navio3[3] = {3,3,3};
+   int navio4[3] = {3,3,3};
    printf("\n\n***batalha naval***\n\n"); // exibição do titulo
     
    printf("   ");
@@ -24,7 +25,7 @@
     }
      printf("\n\n");
      
-       mar[0][0] = navio[0];       //exibição do navio no mapa
+       mar[0][0] = navio[0]; //exibição dos navios inicias no mapa
        mar[0][1] = navio[1];
        mar[0][2] = navio[2];
      
@@ -32,13 +33,38 @@
        mar[4][4] = navio2[0];
        mar[5][4] = navio2[1];
        mar[6][4] = navio2[2];
-
-
-
-     for (int i = 0; i < 10; i++){  // exibição do mapa com as cordenadas e os navios
+       
+       for (int i = 0; i < 10; i++)  // exibição dos navios na diagonal do mapa
+       {
+        for (int j = 0; j < 10; j++)  
+        {
+            if (i == 9 && j == 9) 
+            {
+                mar[9][9] = navio3[0];
+            } 
+            if (i == 8 && j == 8)
+            {
+                mar[8][8] = navio3[1];
+            } 
+            if (i == 7 && j == 7)
+            {
+                mar[7][7] = navio3[2];
+            } if (i == 8 && j == 0 && i + j == 8)
+            {
+              mar[8][0] = navio4[0];
+            } if (i == 7 && j == 1 && i + j == 8)
+            {
+                mar[7][1] = navio4[1];
+            } if (i == 6 && j == 2 && i + j == 8)
+            {
+                mar[6][2] = navio4[2];
+            }
+        }
+       }
+     for (int i = 0; i < linhas; i++){  // exibição do mapa com as cordenadas e os navios
         
         printf(" %d ",i+1);
-        for (int j = 0; j < 10; j++){
+        for (int j = 0; j < colunas; j++){
          printf(" %d ",mar[j][i]);
             
         }
